202816d13eb402e509636bdd4b0dc0edb99c1039
tacticalNuke <gvinbleyd2033@gmail.com>
2016-02-05 10:20:22 +0800

    Delete MainWindow.java

--- src/MainWindow.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class MainWindow extends JFrame {
-    public MainWindow(){
-        super();
-        this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
